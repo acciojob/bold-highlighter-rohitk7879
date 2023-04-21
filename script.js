9223424078740c5651fff609162c90dc86d5cdb6
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-22 00:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
+	
+	for(let i of green){
+		i.style.color = "green";
+	}
 
 
 }
 
-
+let green = document.getElementsByTagName("strong");
 function return_normal() {
     //Write your code here
 
+	for(let i of green){
+		i.style.color = "black";
+	}
     
 }
